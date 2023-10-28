a515525a1e2b9c26879afe9f53defac468d67cf5
John Lombardo <jj.lombardo02@gmail.com>
2023-10-28 12:10:26 -0400

    spike and beginning handboss

--- Assets/scripts/PlayerInteraction.cs
+++ Assets/scripts/PlayerInteraction.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (Input.GetButton("Jump"))
+        {
+            animator.SetTrigger("PlayerInteraction"); // Next... Lock movement, make it so player can only attack.
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void MobilizePlayer() { }
 }
