4a4e18af59698bb5561068151aa02bdca30c4aaf
John Lombardo <jj.lombardo02@gmail.com>
2023-10-29 23:33:47 -0400

    gh

---
+++ Assets/playaudio.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playaudio : MonoBehaviour
+{
+    // array of audiosources
+    
+    //make this an array of audio clips
+    public AudioSource[] audio;
+    
+    public void PlayAudio(int i)
+    {
+        audio[i].Play();
+    }
+}
